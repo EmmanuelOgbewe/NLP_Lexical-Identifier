ad551ea707311cdeb4b4b8a64c5447bdceaf02be
Emmanuel Ogbewe <ed_ehiorobo1997@yahoo.com>
2019-01-19 15:43:53 -0500

    Initial Commit

---
+++ NLP_Project1Tests/NLP_Project1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  NLP_Project1Tests.swift
+//  NLP_Project1Tests
+//
+//  Created by Emmanuel  Ogbewe on 1/19/19.
+//  Copyright © 2019 Emmanuel Ogbewe. All rights reserved.
+//
+
+import XCTest
+@testable import NLP_Project1
+
+class NLP_Project1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
